75ca95788a5d6167c0fb2632e2c790bee4654dac
Yongjoon Kwon <littlesnail0513@gmail.com>
2025-06-02 13:16:45 +0900

    modified a little bit of button style

--- src/components/StartingInterface.jsx
+++ src/components/StartingInterface.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import products from "../data/products.json"; // 상품 데이터 가져오기
 import Header from "./Header";
 
-export const blueButtonStyle = "cursor-pointer text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2";
+export const blueButtonStyle = " px-4 py-2 rounded-xl font-medium text-sm bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-indigo-500 hover:to-blue-500 text-white shadow-md transition-all duration-200 hover:scale-105 active:scale-95";
 
 export default function StartingInterface({ userId, setUserId, isLoggedIn, setIsLoggedIn }) {
     // 페이지 정보
@@ -105,7 +105,7 @@ export default function StartingInterface({ userId, setUserId, isLoggedIn, setIs
                         style={{ transition: "opacity 0.4s" }}
                     >
                         <div className="flex-1 flex flex-col items-center justify-center w-full">
-                            <div className="text-xl font-bold text-center text-gray-800">
+                            <div className="text-4xl font-bold text-center text-gray-800">
                                 {banners[bannerIdx].title}
                             </div>
                             <div className="flex justify-center mt-2">
